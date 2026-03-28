c72aef5c945a8ccf967e1fa1b537174c5e78eabd
azim308 <azim.gfuturetech@gmail.com>
2026-03-28 18:16:20 +0530

    Update auth.protect() call to use function invocation in clerkMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  '/',
-  '/api/webhooks/clerk',
-  '/api/webhooks/stripe'
+  "/",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect();
+    await auth().protect();
   }
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
